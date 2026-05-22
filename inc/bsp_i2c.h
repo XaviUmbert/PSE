adb721ddf999f9a7caa0bde7739c95d63a4f84d5
alimarino <162974719+alimarino@users.noreply.github.com>
2026-05-22 12:26:35 +0200

    Lecturas "funcionan"

--- inc/bsp_i2c.h
+++ inc/bsp_i2c.h
@@ -7,7 +7,6 @@
 #ifndef INC_BSP_I2C_H_
 #define INC_BSP_I2C_H_
 
-#include <stdint.h>
 #include <stdbool.h>
 
 /**
@@ -25,30 +24,12 @@ void BSP_I2C_Init(uint8_t addr);
 bool BSP_I2C_WriteRegister(uint8_t reg, uint8_t data);
 
 /**
- * @brief Write multiple bytes to I2C device starting at a register
- * @param reg Register to write first
- * @param data Bytes to write
- * @param count Number of bytes to write
- * @return true on success
- */
-bool BSP_I2C_WriteMulti(uint8_t reg, const uint8_t *data, uint8_t count);
-
-/**
  * @brief Read register from I2C device
  * @param reg Register to read
  * @param val Value read
  * @return true on success
  */
 bool BSP_I2C_ReadRegister(uint8_t reg, uint8_t *val);
-
-/**
- * @brief Read multiple bytes from I2C device starting at a register
- * @param reg Register to read first
- * @param dst Buffer to store results
- * @param count Number of bytes to read
- * @return true on success
- */
-bool BSP_I2C_ReadMulti(uint8_t reg, uint8_t *dst, uint8_t count);
 
 /**
  * @brief Test function
---
+++ inc/my_i2c.h
@@ -0,0 +1,25 @@
+/*
+ * my_ic2.h
+ *
+ *  Created on: 8 de may. de 2026
+ *      Author: practiques
+ */
+
+#ifndef INC_MY_IC2_H_
+#define INC_MY_IC2_H_
+
+#include <stdbool.h>
+#include "../src/my_i2c.c"
+
+static void INIT_MY_I2C(uint8_t addr);
+
+// WRITE
+static bool WRITE_MY_I2C(uint8_t reg, uint8_t data);
+
+// READ
+static bool READ_MY_I2C(uint8_t reg, uint8_t *val);
+
+// TEST
+static bool TEST_MY_I2C(void);
+
+#endif /* INC_MY_IC2_H_ */
